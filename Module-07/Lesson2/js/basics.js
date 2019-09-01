f7bbeba9c6ddc0e81d34ff72d0b50d21b354e8c1
Koallla <zmiyov2422@gmail.com>
2019-09-01 20:21:16 +0300

    Module-07/Lesson-1/2

---
+++ Module-07/Lesson2/js/basics.js
@@ -0,0 +1,11 @@
+'use script';
+
+const loginButton = document.querySelector('button[data-action="login"]');
+
+document.body.addEventListener('click', handleClick);
+loginButton.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  console.log('target: ', event.target);
+  console.log('currentTarget: ', event.currentTarget);
+}
---
+++ Module-07/Lesson2/js/input-events.js
@@ -0,0 +1,10 @@
+'use script';
+
+const input = document.querySelector('.input-events-example > input');
+const output = document.querySelector('.input-events-example > .output');
+
+input.addEventListener('input', handleInputChange);
+
+function handleInputChange(event) {
+  output.textContent = event.currentTarget.value;
+}
