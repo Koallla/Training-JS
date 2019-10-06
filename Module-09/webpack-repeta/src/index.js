bc3ed8e01f4e1798c6c2a61e7eb45050db6eeab5
Koallla <zmiyov2422@gmail.com>
2019-10-07 00:12:48 +0300

    Module-10

--- Module-09/webpack-repeta/src/index.js
+++ Module-09/webpack-repeta/src/index.js
@@ -1,6 +1,9 @@
 import message from './message';
 import arrow from './arrow';
 import './styles.css';
+import listItemTpl from './list-items.hbs';
+
+// console.log(listItemTpl);
 
 console.log(message);
 console.log(arrow(5, 10));
--- Module-09/webpack-repeta/webpack.config.js
+++ Module-09/webpack-repeta/webpack.config.js
@@ -1,45 +1,57 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackBar = require('webpackbar');
+var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_module/,
-        use: ['babel-loader'],
+const loadModeConfig = env => require(`./build-utils/${env.mode}.config`)(env);
+
+//./src/index.js
+module.exports = env =>
+  webpackMerge(
+    {
+      mode: env.mode,
+      context: path.resolve(__dirname, 'src'),
+      entry: './index.js',
+      // {
+      //   contact: './src/contact.js',
+      //   about: './src/about.js',
+      // },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_module/,
+            use: ['babel-loader'],
+          },
+          {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  name: '[path]/[name].[ext]',
+                  limit: 8192,
+                },
+              },
+            ],
+          },
+          { test: /\.hbs$/, loader: 'handlebars-loader' },
+          {
+            test: /\.html$/,
+            use: 'html-loader',
+          },
         ],
       },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-      inject: true,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-  ],
-  devServer: {
-    port: 9000,
-  },
-  devtool: 'cheap-eval-source-map',
-};
+      plugins: [
+        new CleanWebpackPlugin(),
+        new FriendlyErrorsWebpackPlugin(),
+        new WebpackBar(),
+      ],
+    },
+    loadModeConfig(env),
+  );
